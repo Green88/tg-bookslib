257c537fc5493d079155af83150135726aee6ce0
Green88 <taniagr0808@gmail.com>
2016-08-20 16:51:48 +0300

    client skeleton

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Tania on 12/08/16.
+ */
+import React from 'react';
+import { Route } from 'react-router';
+
+import App from './components/app';
+
+export default (
+    <Route path="/" component={App} >
+    </Route>
+);
