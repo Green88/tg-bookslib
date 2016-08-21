268cdfbc95522c484985503225cc44dac018ddea
tania.grinberg <tania.grinberg@galacoral.com>
2016-08-21 09:19:06 +0300

    -added books component (not working)

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@
  * Created by Tania on 12/08/16.
  */
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import Books from './components/books';
 
 export default (
     <Route path="/" component={App} >
+        <IndexRoute component={Books} />
     </Route>
 );
